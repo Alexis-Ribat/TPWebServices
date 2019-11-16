294694a69d18afd81db4c19ebe59e25c73b2ef70
alrib <alrib@LAPTOP-CBOLI6AJ>
2019-11-16 13:31:09 +0100

    Yes

---
+++ Test/src/main/java/com/example/CarRental/CarRentalApplication.java
@@ -0,0 +1,16 @@
+package com.example.CarRental;
+
+import org.springframework.boot.SpringApplication;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalApplication.class, args);
+	}
+
+}
